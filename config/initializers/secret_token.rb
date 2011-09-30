b3cc155efdf1883f7e988ad30df073ed7a5ee942
Ivan Pankov <ivandr.p@gmail.com>
2011-09-30 23:42:12 +0400

    Создание приложения

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Socpars::Application.config.secret_token = 'ccf4889aa193fb21edd48ccb7ea53f53a5328f51b276e32277d086053d292a16b06d996a8ef83119ccdab07e5f8625723b9418209d6d1eae5c58cacc4116d38c'
